6020a4f17fe664ebac7af8ec1e21d8d250803ce4
Aurélien Hébert <aurrelhebert@gmail.com>
2016-11-03 11:12:29 +0100

    Update ExtensionElastic

--- src/main/java/io/warp10/script/elastic/ExtensionElastic.java
+++ src/main/java/io/warp10/script/elastic/ExtensionElastic.java
@@ -5,23 +5,24 @@ import java.util.Map;
 import java.util.Properties;
 
 import io.warp10.WarpConfig;
-import io.warp10.script.WarpScriptStack;
 import io.warp10.warp.sdk.WarpScriptExtension;
 
 public class ExtensionElastic extends WarpScriptExtension {
 
-  public Map<String, Object> getFunctions() {
-    
+  private final Map<String, Object> functions;
+  
+  public ExtensionElastic() {
     Properties properties = WarpConfig.getProperties();
     ElasticUtils.MAX_TIMEOUT = properties.getProperty(ElasticUtils.MAX_TIMEOUT_CONF, "NO_TIMEOUT");
-    
-    Map<String, Object> functions = new HashMap<String, Object>();
-    //functions.put("HELLOWARP10", new HELLOWARP10("HELLOWARP10"));
-    functions.put("GETELASTIC", new GETELASTIC("GETELASTIC", false));
-    functions.put("MULTIGETELASTIC", new GETELASTIC("MULTIGETELASTIC", true));
-    functions.put("SEARCHELASTIC", new SEARCHELASTIC("SEARCHELASTIC", false));
-    functions.put("METASEARCHELASTIC", new SEARCHELASTIC("METASEARCHELASTIC", true));
-    return functions;
+    this.functions = new HashMap<String, Object>();
+    this.functions.put("GETELASTIC", new GETELASTIC("GETELASTIC", false));
+    this.functions.put("MULTIGETELASTIC", new GETELASTIC("MULTIGETELASTIC", true));
+    this.functions.put("SEARCHELASTIC", new SEARCHELASTIC("SEARCHELASTIC", false));
+    this.functions.put("METASEARCHELASTIC", new SEARCHELASTIC("METASEARCHELASTIC", true));
+  }
+  
+  public Map<String, Object> getFunctions() {
+    return this.functions;
   }
 
 }
